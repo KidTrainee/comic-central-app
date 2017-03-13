41a7a47ffa7472d4167a762ffee19af0ac7cd2f7
Yann Leflour <yannl@bamlab.fr>
2017-03-13 23:50:02 +0100

    Add progress bar

--- src/components/Comic/Comic.component.js
+++ src/components/Comic/Comic.component.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { Component } from 'react';
-import { TouchableOpacity, Image, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, Image, Text, StyleSheet, View } from 'react-native';
 import config from 'comicCentral/src/config';
 import { colors, margins } from 'comicCentral/src/appStyle';
 
@@ -21,7 +21,10 @@ class Comic extends Component {
           style={styles.cover}
           source={{ uri: `${config.host}${comic.coverUrl}` }}
         >
-          <Text style={styles.name}>{comic.name}</Text>
+          <View style={styles.nameContainer}>
+            <View style={styles.nameProgress} />
+            <Text style={styles.name}>{comic.name}</Text>
+          </View>
         </Image>
       </TouchableOpacity>
     );
@@ -48,12 +51,23 @@ const styles = StyleSheet.create({
     backgroundColor: colors.comic.cover,
     justifyContent: 'flex-end',
   },
-  name: {
-    color: colors.comic.name,
-    textAlign: 'center',
+  nameContainer: {
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     paddingVertical: margins.xs,
     marginBottom: margins.sm,
+  },
+  nameProgress: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    width: '0%',
+    backgroundColor: 'rgba(255,255,255,0.5)',
+  },
+  name: {
+    color: colors.comic.name,
+    textAlign: 'center',
+    backgroundColor: 'transparent',
   },
 });
 
